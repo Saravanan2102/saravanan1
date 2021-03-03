b8e771ef32af1fdd9a07b5c50243f564b0093073
saravanan2102 <saravanan100695@gmail.com>
2021-03-03 13:09:13 +0530

    added employee reverse

--- src/org/sample/Employee.java
+++ src/org/sample/Employee.java
@@ -1,5 +1,15 @@
 package org.sample;
 
 public class Employee {
+	
+	static String s = "saravan";
+	
+	public static void main(String[] args) {
+		
+		StringBuffer d = new StringBuffer();
+		d.append(s);
+		d.reverse();
+	}
+	
 
 }
